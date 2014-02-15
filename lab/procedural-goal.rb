c2b1cde8a67c732ea8f99374a5eff51f66133cda
famished.tiger <famished.tiger@yahoo.com>
2014-02-15 20:33:56 +0100

    Added class ProceduralGoal

---
+++ lab/procedural-goal.rb
@@ -0,0 +1,20 @@
+# A finer-grained goal that cannot be decomposed further into subgoals.
+# Such a goal is satisfied by executing a specific procedure (which is itself a serie of steps).
+class ProceduralGoal < Goal
+	# The procedure (i.e. an ordered set of steps) required to satisfy the goal.
+	attr_reader(:procedure)
+
+	# The list of identified outcomes (success or failing) situations after the execution of the goal's procedure
+	attr_reader(:outcomes)
+
+  # Constructor.
+  # [aName] The name of the goal. It must be unique within a dynamic model.  
+  # [aStatement] The goal statement (as a Mustache template text)
+  # [aProcedure] The procedure to execute in order to fulfill the goal.
+	def initialize(aName, aStatement, aProcedure)
+		super(aName, aStatement)
+		@procedure = aProcedure
+		@outcomes = []
+	end
+
+end # class
